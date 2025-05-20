5ae5d6c1019558b0684911a4fd0680393e319bf1
anjani <anjani.8719@gmail.com>
2025-05-20 12:40:03 +0530

    Edit standAloneTest file

--- src/test/java/rahulshettyacademy/tests/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandAloneTest.java
@@ -20,6 +20,7 @@ public class StandAloneTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// New comments are added
+		// Edit StandAloneTest file
 
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
